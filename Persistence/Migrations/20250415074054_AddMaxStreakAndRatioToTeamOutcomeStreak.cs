0d8b9c94eefccd9664045f434188fa9c72fa5181
Jafidan <r.alagezov@gmail.com>
2025-04-15 14:05:57 +0400

    Migration

---
+++ Persistence/Migrations/20250415074054_AddMaxStreakAndRatioToTeamOutcomeStreak.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMaxStreakAndRatioToTeamOutcomeStreak : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxStreak",
+                table: "TeamOutcomeStreaks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Ratio",
+                table: "TeamOutcomeStreaks",
+                type: "decimal(4,3)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxStreak",
+                table: "TeamOutcomeStreaks");
+
+            migrationBuilder.DropColumn(
+                name: "Ratio",
+                table: "TeamOutcomeStreaks");
+        }
+    }
+}
